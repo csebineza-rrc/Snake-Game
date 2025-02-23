ec0f0c15b076019305ed40d19612cb94b26ea54f
csebineza-rrc <csebineza@rrc.ca>
2025-02-22 23:18:23 -0600

    Added necessary components for the snake game.

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ Description: A program that creates the infamous snake game.
 
 __author__ = "Cedrick S"
 __version__ = "2.2.2025"
+
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=700, height=700)
+screen.bgcolor("cyan")
+screen.title("Snake Game")
+
+
+
+
+
+screen.exitonclick()
