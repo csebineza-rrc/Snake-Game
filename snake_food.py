7d109f2368c75154a0b94e7dc1f6acafe831a0fb
csebineza-rrc <csebineza@rrc.ca>
2025-02-26 00:50:14 -0600

    Implemented new methods of the Food class.

--- snake_food.py
+++ snake_food.py
@@ -4,6 +4,7 @@ __author__ = "Cedrick S"
 __version__ = "2.2.2025"
 
 from turtle import Turtle
+import random
 
 class Food (Turtle):
     """Represents the Food class."""
@@ -14,3 +15,11 @@ class Food (Turtle):
         """
 
         super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("black")
+        self.speed("fastest")
+        random_x = random.randint(-670, 670)
+        random_y = random.randint(-670, 670)
+        self.goto(random_x, random_y)
