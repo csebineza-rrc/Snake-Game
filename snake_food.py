92e0a0dec53f4eab832580e607cd2f6f75dd48a7
csebineza-rrc <csebineza@rrc.ca>
2025-02-26 00:41:57 -0600

    Implemented the Food class to inherit from Turtle.

---
+++ snake_food.py
@@ -0,0 +1,16 @@
+"This module defines the Food class."
+
+__author__ = "Cedrick S"
+__version__ = "2.2.2025"
+
+from turtle import Turtle
+
+class Food (Turtle):
+    """Represents the Food class."""
+
+    def __init__(self):
+        """
+        Initializes a new instance of the Food class.
+        """
+
+        super().__init__()
